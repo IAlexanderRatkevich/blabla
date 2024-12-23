86114aa35646e7bcef736af8cb458ad3d6db4b64
Alexander Ratkevich <alexandr.ratkevich.1337@gmail.com>
2024-12-23 14:38:00 +0100

    fix nickname

--- src/interfaces/author.ts
+++ src/interfaces/author.ts
@@ -3,8 +3,6 @@ import {SanityImageSource} from "@sanity/image-url/lib/types/types";
 export interface IAuthor {
   name: string,
   surname: string,
-  nickname: {
-    current: string
-  },
+  nickname: string,
   avatar: SanityImageSource,
 }
